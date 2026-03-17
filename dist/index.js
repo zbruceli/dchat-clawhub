837cb328508f07e82cf5584f6c218a104948c6e9
zbruceli <24281959+zbruceli@users.noreply.github.com>
2026-03-16 19:29:45 -0700

    Revert "Include dist/ in repo and published package"
    This reverts commit 3aeb8416b7da9b03164bde11d0378079e8edd074.

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-export { DchatBot } from "./bot.js";
-export { NknClient } from "./client.js";
-export { IpfsService } from "./ipfs.js";
-export { MediaService } from "./media.js";
-export { MessageDb } from "./db.js";
-export { SafeStorage } from "./storage.js";
-export { encrypt, decrypt } from "./crypto.js";
-//# sourceMappingURL=index.js.map
